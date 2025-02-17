245657b9fa73d7a4e04590f40bf748ca64e57e72
hoodiney <xuduozzd@gmail.com>
2025-02-17 18:19:06 +0800

    update docker_dbg_entry.sh

--- docker_dbg_entry.sh
+++ docker_dbg_entry.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo "deb http://ubuntu.ethz.ch/ubuntu/ focal main restricted" | sudo tee /etc/apt/sources.list
-echo "deb http://ubuntu.ethz.ch/ubuntu/ focal-updates main restricted" | sudo tee -a /etc/apt/sources.list
-echo "deb http://ubuntu.ethz.ch/ubuntu/ focal universe" | sudo tee -a /etc/apt/sources.list
-echo "deb http://ubuntu.ethz.ch/ubuntu/ focal-updates universe" | sudo tee -a /etc/apt/sources.list
+# echo "deb http://ubuntu.ethz.ch/ubuntu/ focal main restricted" | sudo tee /etc/apt/sources.list
+# echo "deb http://ubuntu.ethz.ch/ubuntu/ focal-updates main restricted" | sudo tee -a /etc/apt/sources.list
+# echo "deb http://ubuntu.ethz.ch/ubuntu/ focal universe" | sudo tee -a /etc/apt/sources.list
+# echo "deb http://ubuntu.ethz.ch/ubuntu/ focal-updates universe" | sudo tee -a /etc/apt/sources.list
 cd /home/user/fuzzware_repo || exit
 sudo apt update
 # sudo apt install -y python3-dbg file
-wget -O ~/.gdbinit-gef.py -q https://gef.blah.cat/py
-echo source ~/.gdbinit-gef.py >> ~/.gdbinit
+sudo apt install -y gdb-multiarch
+# wget -O ~/.gdbinit-gef.py -q https://gef.blah.cat/py
+# echo source ~/.gdbinit-gef.py >> ~/.gdbinit
 # UNICORN_DEBUG=yes ./install_local.sh
 ./install_local.sh
 echo "source /home/user/.virtualenvs/fuzzware/bin/activate" | sudo tee -a /home/user/.bashrc
